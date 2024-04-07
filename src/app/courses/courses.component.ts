188819fc1ca800ca8a48e3f90ab712a0b75effc1
snehal-mohite <snehamohite937000@gmail.com>
2024-04-07 13:36:35 +0530

    Basic Changes

---
+++ src/app/courses/courses.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-courses',
+  standalone: true,
+  imports: [],
+  templateUrl: './courses.component.html',
+  styleUrl: './courses.component.css',
+  
+})
+export class CoursesComponent {
+
+}
